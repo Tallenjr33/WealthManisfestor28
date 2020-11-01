7bf537ba0aaa33f17582e659cd256624a383832f
Tallenjr33 <73304886+Tallenjr33@users.noreply.github.com>
2020-11-01 15:17:52 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mad_Lib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is declaring and initializing a variable
+            string adjective = "huge";
+
+            //This is declaring and initializing a variable
+            string noun = "chimpanzee";
+
+            //This is declaring and initializing a variable 
+            string verb = "scurried";
+
+            //This is declaring and initializing a variable
+            string adverb = "rapidly";
+
+            //This is declaring and initializing a variable
+            string adjective2 = "enourmous";
+
+            //This is declaring and initializing a variable
+            string noun2 = "den";
+
+            //This is declaring and initializing a variable
+            string noun3 = "Elephant";
+
+            //This is declaring and initializing a variable 
+            string adjective3 = "enormous";
+
+            //This is declaring and initializing a variable
+            string verb2 = "sprint";
+
+            //This is declaring and initializing a variable
+            string adverb2 = "quickly";
+
+            //This is declaring and initializing a variable
+            string verb3 = "thanked";
+
+            //This is declaring and initializing a variable
+            string adjective4 = "exciting";
+
+            Console.WriteLine(Today i went to the zoo.I saw a + "adjective";
+                + "noun"; jumping up and down in its tree. He + "verb";
+            +"adverb"; AccessedThroughPropertyAttribute the large tunnel that
+            led to its +"adjective2"; +"noun2";.I got some peanuts and passed
+            them through the cage to a giagantic gray +"noun3"; towering above my head.
+            Feeding that animal made me hungry. I went to get a + "adjective3"; scoop of ice
+            cream. It filled my stomach. Afterwards I had to +"verb2"; +"adverb2"; to catch our bus.
+            When I got home I + "verb3"; my mom for a + "adjective4"; day at the zoo.) ;
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
